2478c0b494ec8d08ad87ae0f322c4ab363cdd34d
IIC webteam <webteam2004@gmail.com>
2026-09-02 12:44:25 +0530

    Fix spelling error in project confidence message
    Corrected the spelling of 'confidence' in the text.

--- client/src/pages/sih 2026/sih2026.js
+++ client/src/pages/sih 2026/sih2026.js
@@ -255,7 +255,7 @@ function Sih2026() {
         </h1>
 
         <p className={styles.heroText}>
-          If you have 100% confindence that your project would be shortlisted. Fill the form below, this will provide a chance for re-evaluation of your project.<br> 
+          If you have 100% confindence that your project would be shortlisted. Fill the form below, this will provide a chance for re-evaluation of your project. 
           <b>PROJECT WILL BE EVALUATED, MIGHT BE A CHANCE TO GET SHORTLISTED!</b>
         </p>
 
